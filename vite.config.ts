1d3e86a688ca3781b2b4a61612db59d65cdd2bc5
lencx <cxin1314@gmail.com>
2021-05-29 09:34:01 +0800

    chore: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import ViteRsw from 'vite-plugin-rsw';
+import viteRsw from 'vite-plugin-rsw';
 
 export default defineConfig({
   plugins: [
     reactRefresh(),
     tsconfigPaths(),
-    ViteRsw({
+    viteRsw({
       mode: 'dev',
-      crates: [
-        '@rsw/xi',
-      ],
+      crates: ['@rsw/xi'],
     }),
   ],
 });
